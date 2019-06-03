bd83ba2fbba336871a2cef9a0870238c6428deab
MadDoooog <34088368+MadDoooog@users.noreply.github.com>
2019-06-03 10:06:38 -0500

    创建跑通springboog项目

---
+++ mingrisoft/src/main/java/com/amarsoft/mingri/mingrisoft/bean/TestBean.java
@@ -0,0 +1,12 @@
+package com.amarsoft.mingri.mingrisoft.bean;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestBean {
+
+	public TestBean() {
+		System.out.println("hello world");
+	}
+	
+}
